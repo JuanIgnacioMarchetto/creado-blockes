01bc3e608a4a54ab6a34d345f1a61f92bcb04712
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-16 11:45:22 -0300

    11:45

--- principal.js
+++ principal.js
@@ -3,6 +3,7 @@ const numeroInput = document.getElementById('numero');
 const colorInput = document.getElementById('color');
 const contenedorDivs = document.getElementById('contenedor');
 const botonCrear = document.getElementById('boton-crear');
+const botonArcoiris = document.getElementById('boton-arcoiris');
 
 // Función para crear los divs
 function crearDivs() {
@@ -25,7 +26,29 @@ function crearDivs() {
     div.style.height = '100px';
     contenedorDivs.appendChild(div);
   }
+
+  // Verificar si el número comienza con 7 y mostrar el botón "Arcoíris" si es así
+  if (numero.toString().startsWith('7')) {
+    botonArcoiris.style.display = 'block';
+  } else {
+    botonArcoiris.style.display = 'none';
+  }
 }
 
-// Evento para crear los divs cuando se hace clic en el botón
+// Función para pintar los bloques con colores del arcoíris
+function pintarArcoiris() {
+  const bloques = contenedorDivs.querySelectorAll('div');
+  const coloresArcoiris = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  let indiceColor = 0;
+
+  bloques.forEach((bloque) => {
+    bloque.style.backgroundColor = coloresArcoiris[indiceColor];
+    indiceColor = (indiceColor + 1) % coloresArcoiris.length;
+  });
+}
+
+// Evento para crear los divs cuando se hace clic en el botón "Crear Divs"
 botonCrear.addEventListener('click', crearDivs);
+
+// Evento para pintar los bloques con colores del arcoíris cuando se hace clic en el botón "Arcoíris"
+botonArcoiris.addEventListener('click', pintarArcoiris);
